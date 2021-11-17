8170691e705bbbec6f0d3b3cf7ad6fd4594c5850
Ozkanil <ozkanilhan83@gmail.com>
2021-11-17 14:41:09 +0300

    Initial commit

---
+++ src/PoolArea/Runner.java
@@ -0,0 +1,13 @@
+package PoolArea;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+        Rectangle rectangle= new Rectangle(5, 10);
+        System.out.println(rectangle.getArea());
+        Cuboid cuboid= new Cuboid(5,10,5);
+        System.out.println(cuboid.getArea());
+        System.out.println(cuboid.getVolume());
+    }
+}
